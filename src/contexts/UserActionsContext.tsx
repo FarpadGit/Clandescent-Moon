791ec1924cef308b6756011225dc94af6ed392b5
FarpadGit <farpadmail@gmail.com>
2025-01-30 23:20:53 +0100

    Update UserActionsContext.tsx

--- src/contexts/UserActionsContext.tsx
+++ src/contexts/UserActionsContext.tsx
@@ -9,7 +9,7 @@ import FileSaver from "file-saver";
 // reverse cors proxy required for ki.tc as it doesn't provide an Access-Control-Allow-Origin header for client side requests
 // alternatives:
 // "https://corsproxy.io/?url="
-const REVERSE_PROXY_PREFIX = "https://thingproxy.freeboard.io/fetch/";
+const REVERSE_PROXY_PREFIX = "";
 
 export type userActions =
   | "select"
